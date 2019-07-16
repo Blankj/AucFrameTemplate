a27add7610b051ba99b6c470ced8ed5c12134918
Blankj <blankj@qq.com>
2019-07-16 16:15:49 +0800

    AucFrame 之让你的 Gradle 更智能

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,11 @@
+class Config {
+
+    static applicationId = 'com.blankj.aucframe'            // TODO: MODIFY
+    static appName = 'AucFrame'                             // TODO: MODIFY
+
+    static compileSdkVersion = 29                           // TODO: MODIFY
+    static minSdkVersion = 21                               // TODO: MODIFY
+    static targetSdkVersion = 29                            // TODO: MODIFY
+    static versionCode = 1_000_000                          // TODO: MODIFY
+    static versionName = '1.0.0'// E.g. 1.9.72 => 1,009,072 // TODO: MODIFY
+}
